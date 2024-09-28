b515e91f93ce0a7006fd352b231bd02f33497581
nyascla <lavernia.llorens@gmail.com>
2024-09-28 11:43:23 +0200

    primera intento de arcoiris

--- arcoiris.py
+++ arcoiris.py
@@ -1,14 +1,69 @@
-"""
-El propósito principal es revertir un hash a su valor original
-(por ejemplo, una contraseña).
+import hashlib
+import os
+from datetime import datetime
 
-entrada de 40 bits
-"""
+from logs.logger import get_logger
+
+DIR = "logs/arcoiris"
+os.makedirs(DIR, exist_ok=True)
+FILE = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
+LOGPATH = os.path.join(DIR, FILE)
+
+logger = get_logger(os.path.basename(__file__), LOGPATH)
+
+
+def hash_string(s):
+    """Genera un hash de 40 bits a partir de una cadena."""
+    # convierte la cadena en bytes, algoritmos operan en formato binario.
+    # devuelve el hash en formato hexadecimal.
+    # carácter hexadecimal 4 bits x 10 = 40 bits.
+    return hashlib.sha256(s.encode()).hexdigest()[:2]
+
+
+palabras = ["Mesa", "Silla", "Computadora", "Ventana", "Libro", "Casa", "Perro", "Gato", "Árbol", "Cielo", "Sol",
+            "Luna", "Estrella", "Mar", "Río", "Montaña", "Playa", "Jardín", "Flor", "Fruta", "Comida", "Agua", "Viento",
+            "Nube", "Lluvia", "Fuego", "Tierra", "Camino", "Ciudad", "Pueblo", "Vehículo", "Tren", "Avión", "Bicicleta",
+            "Reloj", "Teléfono", "Música", "Película", "Juego", "Arte", "Pintura", "Fotografía", "Palabra", "Historia",
+            "Ciencia", "Matemáticas", "Geografía", "Tecnología", "Salud", "Sueño"]
+
+
+def do(o):
+    logger.info(f"objetivo: {o}")
+    table = []
+    for palabra in palabras:
+        logger.info(f"fila: {palabra}")
+        dic = dict()
+        table.append(dic)
+
+        p = palabra
+        h = hash_string(p)
+        for _ in range(10000000):
+            dic[p] = h
+            p = h
+            h = hash_string(p)
+
+            if h == o:
+                logger.info(f"coincidencia:"
+                            f"\n\n"
+                            f"hash {o}: {hash_string(p)}"
+                            f"palabra {o}: {hash_string(p)}")
+                return
+
+    for x in table:
+        logger.info(f"{x}")
 
 
 if "__main__" == __name__:
-    pass
+    OBJETIVO = "mama"
+    HASH_OBJETIVO = hash_string(OBJETIVO)
 
+    do(HASH_OBJETIVO)
+
+"""
+DUDAS:
+porque una matriz y no un diccioanrio?
+para que la funcion recodificante si el hash obtenido ya es una secuencia aleatoria
+"""
 
 """
 Require: Una función resumen h y una función recodificante r
